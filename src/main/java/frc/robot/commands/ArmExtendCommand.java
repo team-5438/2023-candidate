a4d11660b6430c8b37234261d909d80498da6a9f
theo! <117363824+theo-real@users.noreply.github.com>
2023-04-02 03:47:24 +0000

    add PID control

--- src/main/java/frc/robot/commands/ArmExtendCommand.java
+++ src/main/java/frc/robot/commands/ArmExtendCommand.java
@@ -12,15 +12,18 @@ public class ArmExtendCommand {
 
     public ArmExtendCommand(ArmSubsystem armSub) {
         armSubsystem = armSub;
-        
+        double maxPositionChange = .1;
+        double positionChange = MathUtil.applyDeadband(controllerSubsystem.operatorLeftJoystickPercent, 0.05) * maxPositionChange;
+        double setpoint = armSubsystem.pivot_encoder.getPosition() + positionChange; // change this maybe
         controllerSubsystem = new ControllerSubsystem();
         //if(controllerSubsystem.operatorLeftJoystickPercent < 0.2 && controllerSubsystem.operatorLeftJoystickPercent > -0.2){
             
           //  armSubsystem.pivot_controller.setReference(.7, ControlType.kPosition);
         //}
         //else{
-            armSubsystem.pivot.set(MathUtil.applyDeadband(controllerSubsystem.operatorLeftJoystickPercent, 0.05));
+            //armSubsystem.pivot.set(MathUtil.applyDeadband(controllerSubsystem.operatorLeftJoystickPercent, 0.05));
             armSubsystem.extender.set(MathUtil.applyDeadband(.25*controllerSubsystem.extenderChange, 0.1));
+            armSubsystem.pivot_controller.setReference(setpoint, ControlType.kPosition);
         //}
     }
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -14,21 +14,21 @@ public class ArmSubsystem {
     public CANSparkMax extender = new CANSparkMax(6, MotorType.kBrushless);
     public SparkMaxAbsoluteEncoder pivot_encoder = pivot.getAbsoluteEncoder(com.revrobotics.SparkMaxAbsoluteEncoder.Type.kDutyCycle);
     public SparkMaxPIDController pivot_controller = pivot.getPIDController();
-    
 
-
+    // Function to convert encoder counts to radians
+        double encoderCountsToRadians(double encoderCounts) {
+            double angleRadians = (encoderCounts / 8192) * (2 * Math.PI);
+            return angleRadians;
+        }
 
     public static ArmFeedforward pivotfeedforward = new ArmFeedforward(0, 1.1, 4.19,0.08);
     public static ElevatorFeedforward extenderfeedforward = new ElevatorFeedforward(0.1, 1.3, 2.3,0.1);
 
     public ArmSubsystem() {
-       /*  pivot_controller.setP(0.1);
+        pivot_controller.setP(0.1); // change all of these
         pivot_controller.setI(0.0001);
         pivot_controller.setD(0.01);
-        pivot_controller.setFF(0);
-        pivot_controller.setOutputRange(-0.5,0.5);*/
-
-        
-
+        pivot_controller.setFF(-pivotfeedforward.calculate(pivot_encoder.getPosition(), 0)); // change this
+        pivot_controller.setOutputRange(-0.5,0.5);
     }
 }
