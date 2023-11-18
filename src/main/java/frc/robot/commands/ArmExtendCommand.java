218b946b6a411b513a8ffc900869df1f38f8a308
Fedor Leonov <leonovf26@students.spprep.org>
2023-11-18 13:28:03 -0500

    add feed forward, and custom PID equation

--- src/main/java/frc/robot/commands/ArmExtendCommand.java
+++ src/main/java/frc/robot/commands/ArmExtendCommand.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ControllerSubsystem;
 
@@ -10,9 +12,27 @@ public class ArmExtendCommand {
 
     public ArmExtendCommand(ArmSubsystem armSub) {
         armSubsystem = armSub;
+
+        double maxArmTravelTime = 32;
+        double maxArmThreshold = (-52.4 + maxArmTravelTime * (-ArmSubsystem.pivotencoder.getVelocity() / 3365));
+        
+        SmartDashboard.putNumber("angle", ArmSubsystem.pivotencoder.getPosition());
+
         
         controllerSubsystem = new ControllerSubsystem();
-        armSubsystem.pivot.set(MathUtil.applyDeadband(controllerSubsystem.operatorLeftJoystickPercent, 0.05));
-        armSubsystem.extender.set(MathUtil.applyDeadband(.25*controllerSubsystem.extenderChange, 0.1));
+
+        double pivot = (MathUtil.applyDeadband(controllerSubsystem.operatorLeftJoystickPercent, 0.05)) - ArmSubsystem.pivotfeedforward.calculate(ArmSubsystem.pivotencoder.getPosition(), 1, 2);
+        SmartDashboard.putNumber("movement", pivot);
+        SmartDashboard.putNumber("velocity", ArmSubsystem.pivotencoder.getVelocity());
+        
+        SmartDashboard.putNumber("MaxArmThreshold", maxArmThreshold);
+
+        
+
+        if ((ArmSubsystem.pivotencoder.getPosition() < -0.1 && pivot > 0) || (ArmSubsystem.pivotencoder.getPosition() > maxArmThreshold && pivot <= 0)) {
+            ArmSubsystem.pivot.set(pivot);
+        } else
+            ArmSubsystem.pivot.set(-ArmSubsystem.pivotfeedforward.calculate(ArmSubsystem.pivotencoder.getPosition(), 1, 2));
+        armSubsystem.extender.set(MathUtil.applyDeadband(.25 * controllerSubsystem.extenderChange, 0.1));
     }
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,20 +1,26 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.SparkMaxPIDController;
 
 public class ArmSubsystem {
-    public CANSparkMax pivot = new CANSparkMax(5, MotorType.kBrushless);
+    public static CANSparkMax pivot = new CANSparkMax(5, MotorType.kBrushless);
     public CANSparkMax extender = new CANSparkMax(6, MotorType.kBrushless);
     public SparkMaxAbsoluteEncoder pivot_encoder = pivot.getAbsoluteEncoder(com.revrobotics.SparkMaxAbsoluteEncoder.Type.kDutyCycle);
     public SparkMaxPIDController pivot_controller = pivot.getPIDController();
+    public static RelativeEncoder pivotencoder = pivot.getEncoder();
 
-    public static ArmFeedforward pivotfeedforward = new ArmFeedforward(0, 1.1, 4.19,0.08);
+    // pid
+    PIDController pid = new PIDController(0, 0, 0);
+
+    public static ArmFeedforward pivotfeedforward = new ArmFeedforward(0, 0.01, 0.01,0.01);
     public static ElevatorFeedforward extenderfeedforward = new ElevatorFeedforward(0.1, 1.3, 2.3,0.1);
 
     public ArmSubsystem() {
